54786c9095ea3ea5ce218325d7065b44d193daf7
6君 <478385373@qq.com>
2021-01-12 23:49:59 +0800

    016初遇urp后处理

---
+++ Assets/URP/016建立自己的后处理系统并调整屏幕的亮度饱和度对比度/MyFeature.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class MyFeature : ScriptableRendererFeature
+{
+    [System.Serializable]
+    public class mysetting
+    {
+        public RenderPassEvent passEvent = RenderPassEvent.AfterRenderingTransparents;
+        public Material mymat;
+        public int matpassindex = -1;
+    }
+
+    public mysetting setting = new mysetting();
+
+    public override void Create()
+    {
+        //计算材质球里总的pass数，如果没有则为1
+        int passint = setting.mymat == null ? 1 : setting.mymat.passCount - 1;
+        //把设置里的pass的id限制在-1到材质的最大pass
+        setting.matpassindex = Mathf.Clamp(setting.matpassindex, -1, passint);
+        //实例化一下并传参
+        
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
